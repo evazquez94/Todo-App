40a4e3cb90f0184a347805283da23f833cd96c19
evazquez94 <eduardo940531@gmail.com>
2021-03-31 19:45:42 -0400

    Add ReactBootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Editor, EditorState, RichUtils, convertToRaw} from 'draft-js';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import 'draft-js/dist/Draft.css';
 import './Home.css';
 
@@ -28,16 +30,14 @@ function Home() {
   }
 
   const handleClick = (ev) => {
+    // Save file for download for the input
     const blocks = convertToRaw(editorState.getCurrentContent()).blocks;
     const description = blocks
       .map(block => (!block.text.trim() && "\n") || block.text)
       .join("\n");
 
-    console.log(description);
-
     const element = document.createElement("a");
-    const file = new Blob([description],    
-                {type: 'text/plain;charset=utf-8'});
+    const file = new Blob([description], {type: 'text/plain;charset=utf-8'});
     element.href = URL.createObjectURL(file);
     element.download = "myFile.txt";
     document.body.appendChild(element);
@@ -47,37 +47,43 @@ function Home() {
 
   return (
     <>
-      <div className="card">
-        <h1>Todo App</h1>              
-      </div>
+      <Container>        
+        <h1 className="title">Todo App</h1>              
 
-      <div className="card">
-        <div className="editor">
-          <div className="options">
-            <button onClick={() => setStyleLine('BOLD')}>Bold</button>
-            <button onClick={() => setStyleLine('ITALIC')}>Italic</button>
-            <button onClick={() => setStyleLine('CODE')}>Code</button>
-            <button onClick={() => setStyleLine('UNDERLINE')}>Underline</button>
-            <button onClick={() => toggleTypeBlock('header-one')}>H1</button>
-            <button onClick={() => toggleTypeBlock('header-two')}>H2</button>
-            <button onClick={() => toggleTypeBlock('header-three')}>H3</button>
-            <button onClick={() => toggleTypeBlock('header-four')}>H4</button>
-            <button onClick={() => toggleTypeBlock('header-five')}>H5</button>
-            <button onClick={() => toggleTypeBlock('header-six')}>H6</button>            
-            <button onClick={() => toggleTypeBlock('unordered-list-item')}>UL</button>
-            <button onClick={() => toggleTypeBlock('ordered-list-item')}>OL</button>
-          </div>
-          <Editor         
-            editorState={editorState} 
-            onChange={setEditorState}
-            handleKeyCommand={handleKeyCommand}   
-          />
-        </div>
+        <Row>
+          <Col md={{ span: 8, offset: 2 }} sm={12} xs={12}>
+            <div className="editor">
+              <div className="options">
+                <button onClick={() => setStyleLine('BOLD')}>Bold</button>
+                <button onClick={() => setStyleLine('ITALIC')}>Italic</button>                
+                <button onClick={() => setStyleLine('UNDERLINE')}>Underline</button>
+                <button onClick={() => toggleTypeBlock('header-one')}>H1</button>
+                <button onClick={() => toggleTypeBlock('header-two')}>H2</button>
+                <button onClick={() => toggleTypeBlock('header-three')}>H3</button>
+                <button onClick={() => toggleTypeBlock('header-four')}>H4</button>
+                <button onClick={() => toggleTypeBlock('header-five')}>H5</button>
+                <button onClick={() => toggleTypeBlock('header-six')}>H6</button>            
+                <button onClick={() => toggleTypeBlock('unordered-list-item')}>UL</button>
+                <button onClick={() => toggleTypeBlock('ordered-list-item')}>OL</button>
+              </div>
+              <Editor         
+                editorState={editorState} 
+                onChange={setEditorState}
+                handleKeyCommand={handleKeyCommand}   
+              />  
+            </div> 
+          </Col>       
+        </Row>
 
-        <div className="card">
-          <button className="button" onClick={handleClick}>Save Text</button>
-        </div>
-      </div>
+        <Row>
+          <Col md={{ span: 8, offset: 2 }} sm={12} xs={12}>
+            <Button variant="outline-primary" className="saveButton" onClick={handleClick}>
+              Save as a text file
+            </Button>
+          </Col>
+        </Row>
+
+      </Container>
 
     </>
   )
