97896fa9a27189485599a4eb5a0e7334256e591a
evazquez94 <eduardo940531@gmail.com>
2021-04-04 20:12:52 -0400

    Add create todo and todo list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,74 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "./Home.css";
 
-function Home() {
-  const [todo, setTodo] = React.useState([]);
-  const [input, setInput] = React.useState("");
+function Todo({ todo }) {
+  return (
+    <div style={{ textDecoration: todo.completed ? "line-through" : "" }}>
+      <Row className="todo-item">
+        <Col
+          xs={{ span: 10, offset: 1 }}
+          sm={{ span: 10, offset: 1 }}
+          lg={{ span: 8, offset: 2 }}
+        >
+          <li>{todo.title}</li>
+        </Col>
+        {/* <Col xs={6} sm={6} lg={1}>
+          <i className="far fa-edit" onClick={edit} />
+        </Col>
+        <Col xs={6} sm={6} lg={1}>
+          <i className="fas fa-trash" />
+        </Col> */}
+      </Row>
+    </div>
+  );
+}
 
-  const addTodo = () => {
-    let newTodo = todo;
-    newTodo.push(input);
-    setTodo(newTodo);
-    setInput("");
+function CreateTodo({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+
+    addTodo(value);
+    setValue("");
   };
 
-  const onChangeInput = (event) => {
-    setInput(event.target.value);
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        placeholder="Add a new task"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function Home() {
+  const [todo, setTodo] = React.useState([
+    {
+      title: "Do homework",
+      completed: false,
+    },
+    {
+      title: "Do exercises",
+      completed: true,
+    },
+  ]);
+
+  const addTodo = (title) => {
+    const newTodo = [...todo, { title, completed: false }];
+    setTodo(newTodo);
   };
 
   return (
     <>
       <Container>
-        <h1 className="title">To do App</h1>
+        <h1 className="title">TODO LIST</h1>
 
         <Row>
           <Col
@@ -28,43 +76,19 @@ function Home() {
             md={{ span: 8, offset: 2 }}
             xs={{ span: 10, offset: 1 }}
           >
-            <ul>
-              {todo.length > 0 ? (
-                todo.map((todo) => {
-                  return (
-                    <Row className="todo-item">
-                      <Col xs={10} sm={10} lg={{span: 8, offset:2}}>
-                        <li key={todo}>{todo}</li>
-                      </Col>
-                      <Col xs={6} sm={6} lg={1}>
-                        <i className="far fa-edit" onClick={()=>console.log(todo)}/>
-                      </Col>
-                      <Col xs={6} sm={6} lg={1}>
-                        <i className="fas fa-trash" />
-                      </Col>
-                    </Row>
-                  );
-                })
-              ) : (
-                <>
-                  <h3>No to do to show</h3>
-                  <p>Add some task in the input below</p>
-                </>
-              )}
-            </ul>
+            {todo.map((todo, index) => (
+              <Todo todo={todo} index={index} key={index} />
+            ))}
           </Col>
         </Row>
 
         <Row>
-          <Col className="add-todo">
-            <input type="text" value={input} onChange={onChangeInput} />
-          </Col>
-        </Row>
-        <Row>
-          <Col className="add-todo-button">
-            <Button variant="outline-primary" onClick={addTodo}>
-              Add Todo
-            </Button>
+          <Col
+            className="add-todo"
+            md={{ span: 8, offset: 2 }}
+            xs={{ span: 10, offset: 1 }}
+          >
+            <CreateTodo addTodo={addTodo} />
           </Col>
         </Row>
       </Container>
